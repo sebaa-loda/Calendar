0f40f9531bc2825d6906b873555eee30fd2e2562
Sebaa <sebaamdq96@gmail.com>
2024-04-03 15:34:14 -0300

    add big calendar

---
+++ src/calendar/index.js
@@ -0,0 +1,4 @@
+
+
+export * from './pages/CalendarPage'
+export * from './components/Navbar'
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { Navbar } from '../'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-US'
 
 export const CalendarPage = () => {
   return (
-    <div>CalendarPage</div>
+    <>
+      <Navbar />
+
+
+    </>
   )
 }
