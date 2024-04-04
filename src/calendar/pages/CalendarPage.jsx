4d419b9dfe8e8351bd46459cd291cc0a99c4a4db
Sebaa <sebaamdq96@gmail.com>
2024-04-04 20:01:17 -0300

    add big calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,17 +1,38 @@
-import { Navbar } from '../'
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import enUS from 'date-fns/locale/en-US'
+import { Navbar } from "../";
+import { Calendar } from "react-big-calendar";
+import { addHours} from "date-fns";
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import {localizer , getMessagesEs} from '../../helpers'
+
+const events = [{
+  title: 'cumpleaños del jefe',
+  note: 'Hay que comprar un regalo',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+
+}]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+
+  }
+
+
   return (
     <>
       <Navbar />
 
-
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc (100vh - 80px)' }}
+        messages={getMessagesEs()}
+        eventPropGetter={eventStyleGetter}
+      />
     </>
-  )
-}
+  );
+};
